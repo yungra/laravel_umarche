bf2e3997cce388c6a1d473e77b585696cd00894b
yungra <yhuga01@gmail.com>
2022-10-20 14:23:12 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -18,14 +19,14 @@ class ShopController extends Controller
             // dd(Auth::id()); //数字
 
             $id = $request->route()->parameter('shop'); //shopのid取得
-            if(!is_null($id)){ // null判定
+            if (!is_null($id)) { // null判定
                 $shopsOwnerId = Shop::findOrFail($id)->owner->id;
                 $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
                 $ownerId = Auth::id();
-                if($shopId !== $ownerId){ // 同じでなかったら
+                if ($shopId !== $ownerId) { // 同じでなかったら
                     abort(404); // 404画面表示
                 }
-            } 
+            }
 
             return $next($request);
         });
@@ -42,11 +43,20 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
-    }
+        $imageFile = $request->image; //一時保存
 
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+        }
+
+        return redirect()->route('owner.shops.index');
+
+    }
 }
