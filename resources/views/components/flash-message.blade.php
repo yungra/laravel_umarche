faa89821360a1afb5cfc5b3444e98b825cb530ad
yungra <yhuga01@gmail.com>
2022-10-21 22:24:10 +0900

    image delete

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -4,7 +4,7 @@ if(session('status') === 'info'){ $bgColor = 'bg-blue-300';}
 if(session('status') === 'alert'){ $bgColor = 'bg-red-500'; }
 @endphp
 @if(session('message'))
- <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+ <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 text-white">
  {{ session('message' )}}
  </div>
 @endif 
