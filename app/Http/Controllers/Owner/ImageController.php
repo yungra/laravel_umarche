e723504329994d3e060e8eb9bbcf6fd26e688ea4
yungra <yhuga01@gmail.com>
2022-10-21 21:36:37 +0900

    store

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 
 
@@ -37,7 +38,6 @@ class ImageController extends Controller
         $images = Image::where('owner_id', Auth::id())
         ->orderBy('updated_at', 'desc')
         ->paginate(20); 
-
         return view('owner.images.index', compact('images'));
     }
 
@@ -59,7 +59,22 @@ class ImageController extends Controller
      */
     public function store(UploadImageRequest $request)
     {
-        dd($request);
+        $imageFiles = $request->file('files');
+        if(!is_null($imageFiles)){
+            foreach($imageFiles as $imageFile){
+                $fileNameToStore = ImageService::upload($imageFile, 'products');
+                Image::create([
+                    'owner_id' => Auth::id(),
+                    'filename' => $fileNameToStore,
+                ]);
+            }
+        }
+        return redirect()
+        ->route('owner.images.index')
+        ->with([
+            'message' => '画像登録を実施しました。',
+            'status' => 'info'
+        ]);;
     }
 
     /**
