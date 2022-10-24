2aa40d3142febca5d000fcfb788d10cf08f825e8
yungra <yhuga01@gmail.com>
2022-10-25 07:16:15 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -5,25 +5,44 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    public function add (Request $request)
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach ($products as $product) {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        // dd($products, $totalPrice);
+
+        return view(
+            'user.cart',
+            compact('products', 'totalPrice')
+        );
+    }
+
+    public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
-        ->where('user_id', Auth::id())->first();
+            ->where('user_id', Auth::id())->first();
 
-        if($itemInCart){
+        if ($itemInCart) {
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
-        } else{
+        } else {
             Cart::create([
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
                 'quantity' => $request->quantity,
             ]);
         }
-        dd('テスト');
+        return redirect()->route('user.cart.index');
     }
 }
