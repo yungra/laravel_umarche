0fca9117050938b14426225eb35805757c2b06f1
yungra <yhuga01@gmail.com>
2022-10-26 18:22:12 +0900

    表示順

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -81,27 +81,46 @@ class Product extends Model
             ->having('quantity', '>', 1);
 
         return $query
-        ->joinSub($stocks, 'stock', function ($join) {
-            $join->on('products.id', '=', 'stock.product_id');
-        })
-        ->join('shops', 'products.shop_id', '=', 'shops.id')
-        ->join(
-            'secondary_categories',
-            'products.secondary_category_id',
-            '=',
-            'secondary_categories.id'
-        )
-        ->join('images as image1', 'products.image1', '=', 'image1.id')
-        ->where('shops.is_selling', true)
-        ->where('products.is_selling', true)
-        ->select(
-            'products.id as id',
-            'products.name as name',
-            'products.price',
-            'products.sort_order as sort_order',
-            'products.information',
-            'secondary_categories.name as category',
-            'image1.filename as filename'
-        );
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join(
+                'secondary_categories',
+                'products.secondary_category_id',
+                '=',
+                'secondary_categories.id'
+            )
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id as id',
+                'products.name as name',
+                'products.price',
+                'products.sort_order as sort_order',
+                'products.information',
+                'secondary_categories.name as category',
+                'image1.filename as filename'
+            );
+    }
+
+    public function scopeSortOrder($query, $sortOrder)
+    {
+        if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['recommend']) {
+            return $query->orderBy('sort_order', 'asc');
+        }
+        if ($sortOrder === \Constant::SORT_ORDER['higherPrice']) {
+            return $query->orderBy('price', 'desc');
+        }
+        if ($sortOrder === \Constant::SORT_ORDER['lowerPrice']) {
+            return $query->orderBy('price', 'asc');
+        }
+        if ($sortOrder === \Constant::SORT_ORDER['later']) {
+            return $query->orderBy('products.created_at', 'desc');
+        }
+        if ($sortOrder === \Constant::SORT_ORDER['older']) {
+            return $query->orderBy('products.created_at', 'asc');
+        }
     }
 }
