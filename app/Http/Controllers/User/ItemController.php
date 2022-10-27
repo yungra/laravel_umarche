77a86b384686116391169688f5da058cbd461bfb
yungra <yhuga01@gmail.com>
2022-10-27 16:51:07 +0900

    ユーザ向けメール送信

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -42,7 +42,7 @@ class ItemController extends Controller
         // ->send(new TestMail());
 
         //非同期に送信
-        SendThanksMail::dispatch();
+        // SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
             ->get();
--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -17,12 +17,12 @@ class CartService
       $values = array_values($owner);
       $keys = ['ownerName', 'email'];
       $ownerInfo = array_combine($keys, $values);
-    
+
       $product = Product::where('id', $item->product_id)
-      ->select('id', 'name', 'price')->get()->toArray();
+        ->select('id', 'name', 'price')->get()->toArray();
 
       $quantity = Cart::where('product_id', $item->product_id)
-      ->select('quantity')->get()->toArray();
+        ->select('quantity')->get()->toArray();
 
       $result = array_merge($product[0], $ownerInfo, $quantity[0]);
 
